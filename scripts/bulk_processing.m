fb0976e3f4427712d15b062880ae477d49eaf5fa
Miquel Tutusaus <149240218+mtutusaus@users.noreply.github.com>
2025-11-26 16:40:42 +0100

    Update bulk_processing.m

--- scripts/bulk_processing.m
+++ scripts/bulk_processing.m
@@ -1,3 +1,38 @@
+%% Initialization and processing
+close all;  clear; clc
+
+% Import waveforms, multiselect is enable so select waveforms and Vge at the same time
+[filenames, path] = uigetfile('*.csv', 'Select waveform and VGE files', 'MultiSelect', 'on');
+[time, Vce_clamped, Ic] = importCSV(fullfile(path, filenames{1}));
+data = importdata(fullfile(path, filenames{2}));
+Vge = data.data(:, 2); clear data;
+
+% Peltier initial temperature
+Tpeltier = 50;
+
+% Import LUT and convert to struct
+load("LUT.mat");
+LUT.current = current;
+LUT.temp = temp;
+LUT.Vce_interpolation_current = Vce_interpolation_current;
+
+% Offset, Ls and thermal impedance correction parameters
+fit_offset = [1e-3 0]; % Polynomial coefficients
+Ls = 15e-9; % Stray inductance in H
+Zth = 0.1; % Thermal resistance J-C in K/W at 250μs single pulse (from datasheet)
+
+% Processing window mask
+wndw_left = 0.25; % in microseconds
+wndw_right = 0.25; % in microseconds
+
+% Uncertainties
+sigma.Ic = 1e-3; % Uncertainty on the measurement of Ic
+sigma.Vce = 1e-3; % Uncertainty on the measurement of Vce
+sigma.current_table = 10e-3; % Uncertainty on the measurement of Ic on the curve tracer
+sigma.Vce_table = 1e-3; % Uncertainty on the measurement of Vce on the curve tracer
+sigma.temp_fit = 10e-3; % Uncertainty on the measurement of Tj on the curve tracer
+
+
 fprintf('--- Analysis at maximum Ic --- \n');
 for i=1:2:length(filenames)
     % Import data
